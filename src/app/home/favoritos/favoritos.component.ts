9471fa5f07743d46d65d7d01ca00a109ab8b656e
05562849259 <kaiomm2000@gmail.com>
2024-02-23 19:44:03 -0300

    tela de favoritos update

--- src/app/home/favoritos/favoritos.component.ts
+++ src/app/home/favoritos/favoritos.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatList, MatListItem } from '@angular/material/list';
+import { CardProdutoFavoritoComponent } from '../../components/card-produto-favorito/card-produto-favorito.component';
 
 @Component({
   selector: 'app-favoritos',
   standalone: true,
-  imports: [],
+  imports: [MatList, MatListItem, CardProdutoFavoritoComponent],
   templateUrl: './favoritos.component.html',
   styleUrl: './favoritos.component.css'
 })
 export class FavoritosComponent {
+    favorites = [
+      {
+      },
+    ];
+
 
 }
