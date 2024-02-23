f2b9bc4c8b80b48c842785654e478604ef91c0c6
05562849259 <kaiomm2000@gmail.com>
2024-02-23 16:16:36 -0300

    c curtidas add

---
+++ src/app/home/curtidas/curtidas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curtidas',
+  standalone: true,
+  imports: [],
+  templateUrl: './curtidas.component.html',
+  styleUrl: './curtidas.component.css'
+})
+export class CurtidasComponent {
+
+}
