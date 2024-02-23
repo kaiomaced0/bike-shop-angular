d91b3c7c04c80cb4e5a618eca0f2e436314497ed
05562849259 <kaiomm2000@gmail.com>
2024-02-23 19:44:22 -0300

    add carrossel na home

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { CardProdutoComponent } from '../../components/card-produto/card-produto.component';
+import { CarroselHomeComponent } from '../../components/carrosel-home/carrosel-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, CardProdutoComponent],
+  imports: [MatCardModule, CardProdutoComponent, CarroselHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
