06adbab71282d20295bf32e5c556b63dde97b8b6
JPMatrix <joaopedrocvmatrix@gmail.com>
2024-02-23 21:59:58 -0300

    first-detail

--- src/app/home/detail/detail.component.ts
+++ src/app/home/detail/detail.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
+
 export class DetailComponent {
+    product = {
+    name: 'Bike Adulto',
+    price: 100,
+    // Inclua outras propriedades necessárias
+  };
+
+  changeImage(imageUrl: string): void {
+    // Lógica para alterar a imagem principal
+    console.log('Mudar imagem principal para:', "bike.jpg");
+  }
 
 }
