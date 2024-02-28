1e9a41cd5bcc3d0ad91e136f541752b645f292a7
05562849259 <kaiomm2000@gmail.com>
2024-02-28 18:25:28 -0300

    add component dados da conta

---
+++ src/app/home/conta/dados/dados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dados',
+  standalone: true,
+  imports: [],
+  templateUrl: './dados.component.html',
+  styleUrl: './dados.component.css'
+})
+export class DadosComponent {
+
+}
