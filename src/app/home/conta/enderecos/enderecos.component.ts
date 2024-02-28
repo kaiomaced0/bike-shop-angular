7596b2e0351ef09fbc390d4e84ff3a09b3c4d44f
05562849259 <kaiomm2000@gmail.com>
2024-02-28 18:25:42 -0300

    add c enderecos conta

---
+++ src/app/home/conta/enderecos/enderecos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enderecos',
+  standalone: true,
+  imports: [],
+  templateUrl: './enderecos.component.html',
+  styleUrl: './enderecos.component.css'
+})
+export class EnderecosComponent {
+
+}
