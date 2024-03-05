b19f4e5d2993b19141c4bbd0d0329af23704c032
05562849259 <kaiomm2000@gmail.com>
2024-03-05 17:56:39 -0300

    ajustes icones produto favorito

--- src/app/components/card-produto-favorito/card-produto-favorito.component.ts
+++ src/app/components/card-produto-favorito/card-produto-favorito.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { MatList, MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'app-card-produto-favorito',
   standalone: true,
-  imports: [MatList, MatListItem],
+  imports: [MatList, MatListItem, MatIcon],
   templateUrl: './card-produto-favorito.component.html',
   styleUrl: './card-produto-favorito.component.css'
 })
