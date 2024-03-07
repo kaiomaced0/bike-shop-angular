58e301190323615433c12a6dfbac1c7a68948bb6
05562849259 <kaiomm2000@gmail.com>
2024-03-07 20:10:54 -0300

    card produto home ajustado

--- src/app/components/card-produto-favorito/card-produto-favorito.component.ts
+++ src/app/components/card-produto-favorito/card-produto-favorito.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatList, MatListItem } from '@angular/material/list';
 
@@ -10,5 +10,11 @@ import { MatList, MatListItem } from '@angular/material/list';
   styleUrl: './card-produto-favorito.component.css'
 })
 export class CardProdutoFavoritoComponent {
+  @Input() id?: string; // Valor padrão para o id
+  @Input() imageUrl?: string;
+  @Input() title?: string; // Valor padrão para o título
+  @Input() price?: number; // Valor padrão para o preço
+  @Input() stars?: number; // Valor padrão para a quantidade de estrelas
+  @Input() description?: string;
 
 }
