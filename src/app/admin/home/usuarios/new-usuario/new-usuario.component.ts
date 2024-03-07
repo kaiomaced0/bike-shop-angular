eef80247cf5148b95876e4acbd5faed544afc735
05562849259 <kaiomm2000@gmail.com>
2024-03-07 20:10:24 -0300

    impl c new produto admin

--- src/app/admin/home/usuarios/new-usuario/new-usuario.component.ts
+++ src/app/admin/home/usuarios/new-usuario/new-usuario.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-usuario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-usuario.component.html',
   styleUrl: './new-usuario.component.css'
 })
 export class NewUsuarioComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(form: any) {
+    console.log('Formulário enviado:', form.value);
+  }
+
+  adicionarUsuario() {
+    this.router.navigate(['/admin/usuarios']);
+  }
 }
