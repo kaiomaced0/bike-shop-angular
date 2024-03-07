4b400e18db6594f871259bf4d0461b8c3d0dedfc
05562849259 <kaiomm2000@gmail.com>
2024-03-07 20:09:31 -0300

    impl c new produto admin

--- src/app/admin/home/produtos/new-produto/new-produto.component.ts
+++ src/app/admin/home/produtos/new-produto/new-produto.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-produto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-produto.component.html',
   styleUrl: './new-produto.component.css'
 })
-export class NewProdutoComponent {
+export class NewProdutoComponent{
+  constructor(private router: Router) {}
+  categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4', 'Categoria 5'];
 
+  onSubmit(form: any) {
+    console.log('Dados do Formulário:', form.value);
+    // Aqui você pode adicionar a lógica para salvar os dados do formulário
+  }
+  adicionarProduto() {
+    this.router.navigate(['/admin/produtos']);
+  }
 }
