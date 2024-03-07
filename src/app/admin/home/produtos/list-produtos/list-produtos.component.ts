85168cb7909e8d51ddd45744dcbe5eb4afa567d8
05562849259 <kaiomm2000@gmail.com>
2024-03-07 20:09:14 -0300

    impl c listproduto admin

--- src/app/admin/home/produtos/list-produtos/list-produtos.component.ts
+++ src/app/admin/home/produtos/list-produtos/list-produtos.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-produtos',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, MatButton],
   templateUrl: './list-produtos.component.html',
   styleUrl: './list-produtos.component.css'
 })
 export class ListProdutosComponent {
+  constructor(private router: Router) {}
+
+  produtos = [
+    { id: 1, nome: 'Produto 1', preco: 100.00, categoria: 'Categoria A', quantidadeEstoque: 10, imageUrl: "https://via.placeholder.com/220x250?text=Produto+1" },
+    { id: 2, nome: 'Produto 2', preco: 200.00, categoria: 'Categoria B', quantidadeEstoque: 20,
+    imageUrl: "https://via.placeholder.com/220x250?text=Produto+1"},
+    // Adicione mais produtos conforme necessário
+  ];
+
+  irParaNewProduto() {
+    this.router.navigate(['/admin/produtos/new']);
+  }
+  editarProduto(produtoId: number) {
+    this.router.navigate(['/admin/produtos/edit']);
+  }
+
+  excluirProduto(produtoId: number) {
+    // Lógica para excluir o produto
+  }
 
 }
--- src/app/admin/home/produtos/produtos.module.ts
+++ src/app/admin/home/produtos/produtos.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProdutosRoutingModule } from './produtos-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
-  imports: [
+  imports: [ReactiveFormsModule,
     CommonModule,
     ProdutosRoutingModule
   ]
