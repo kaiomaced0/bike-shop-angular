b0101c09a0c499f11ac475e77bd6fe666e11b29f
05562849259 <kaiomm2000@gmail.com>
2024-03-08 16:25:07 -0300

    impl new categoria admin

--- src/app/admin/home/categorias/new-categoria/new-categoria.component.ts
+++ src/app/admin/home/categorias/new-categoria/new-categoria.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-categoria',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-categoria.component.html',
   styleUrl: './new-categoria.component.css'
 })
 export class NewCategoriaComponent {
 
+  constructor(private router: Router) {}
+
+  onSubmit(form: any) {
+    console.log('Dados do Formulário:', form.value);
+    // Aqui você pode adicionar a lógica para salvar os dados do formulário
+  }
+  adicionarCategoria() {
+    this.router.navigate(['/admin/categorias']);
+  }
 }
