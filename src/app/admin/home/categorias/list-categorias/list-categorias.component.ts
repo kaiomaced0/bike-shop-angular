939aba880d35449ea3b10c84e6caa8a4577bcf79
05562849259 <kaiomm2000@gmail.com>
2024-03-08 16:24:56 -0300

    impl list categorias

--- src/app/admin/home/categorias/list-categorias/list-categorias.component.ts
+++ src/app/admin/home/categorias/list-categorias/list-categorias.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-categorias',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, MatButton],
   templateUrl: './list-categorias.component.html',
   styleUrl: './list-categorias.component.css'
 })
 export class ListCategoriasComponent {
+  constructor(private router: Router) {}
 
+  categorias = [
+    {"id": 1, "nome": "categoria1", "produtos": 15},
+    {"id": 2, "nome": "categoria2", "produtos": 23},
+    {"id": 3, "nome": "categoria3", "produtos": 10},
+    {"id": 4, "nome": "categoria4", "produtos": 45},
+    {"id": 5, "nome": "categoria5", "produtos": 1},
+  ]
+  irParaNewCategoria() {
+    this.router.navigate(['/admin/categorias/new']);
+  }
+  editarCategoria(produtoId: number) {
+    this.router.navigate(['/admin/categorias/edit']);
+  }
+
+  excluirCategoria(produtoId: number) {
+    // Lógica para excluir o produto
+  }
 }
