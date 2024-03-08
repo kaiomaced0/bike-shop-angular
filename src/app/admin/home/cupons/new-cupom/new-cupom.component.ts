16c7bf3245a02b81931b0b2cd217691285e1a0b5
05562849259 <kaiomm2000@gmail.com>
2024-03-08 16:25:30 -0300

    impl new cupom admin

--- src/app/admin/home/cupons/new-cupom/new-cupom.component.ts
+++ src/app/admin/home/cupons/new-cupom/new-cupom.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-cupom',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-cupom.component.html',
   styleUrl: './new-cupom.component.css'
 })
 export class NewCupomComponent {
+  constructor(private router: Router) {}
+  categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3', 'Categoria 4', 'Categoria 5'];
+
+  onSubmit(form: any) {
+    console.log('Dados do Formulário:', form.value);
+    // Aqui você pode adicionar a lógica para salvar os dados do formulário
+  }
+  adicionarCupom() {
+    this.router.navigate(['/admin/cupons']);
+  }
 
 }
