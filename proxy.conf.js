ce65678451aed130a7752e45a83f43498bd7f3e9
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-05 07:56:18 -0300

    proxy para nao ter erro de cors

--- proxy.conf.js
+++ proxy.conf.js
@@ -4,6 +4,72 @@ const PROXY_CONF = [
     target: 'http://localhost:8080/',
     secure: false,
     logLevel: 'debug'
+  },
+  {
+    context: ['/produto/admin'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/bike'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/usuario'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/ferramenta'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/marca'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/cidade'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/pneu'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/freio'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/cupom'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/categoria'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/compra'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
   }
 ];
 
