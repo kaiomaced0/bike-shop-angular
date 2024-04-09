ca25ea81781cfceb6a7e41f8df8cace5932c3ee3
05562849259 <kaiomm2000@gmail.com>
2024-04-09 19:54:51 -0300

    adicionando snackbar e ajustes css

--- src/app/admin/home/usuarios/new-usuario/new-usuario.component.ts
+++ src/app/admin/home/usuarios/new-usuario/new-usuario.component.ts
@@ -1,33 +1,42 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from '../../../../models/usuario.model';
 import { UsuarioService } from '../../../../services/usuario/usuario.service';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-usuario',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatFormField, MatLabel, ReactiveFormsModule, MatButton],
   templateUrl: './new-usuario.component.html',
   styleUrl: './new-usuario.component.css'
 })
 export class NewUsuarioComponent {
   usuario?: Usuario = new Usuario();
-
-  constructor(private router: Router, private service: UsuarioService) {}
-  onSubmit(form: any) {
-    console.log('Dados do Formulário:', form.value);
+  constructor(private router: Router, private service: UsuarioService, private snackBar: MatSnackBar) {
+  }
+  cancelar(){
+    this.router.navigate(['/admin/usuarios']);
   }
   adicionarUsuario() {
     this.service.insert(this.usuario!).subscribe({
       next: (usuarioadd) => {
         console.log('Usuario adicionado com sucesso:', usuarioadd);
         this.router.navigate(['/admin/usuarios']);
+        this.snackBar.open('Usuario adicionado!', 'Fechar', {
+          duration: 2000,
+        });
       },
       error: (erro) => {
         console.error('Erro ao adicionar Usuario:', erro);
-        // Trate erros, como exibir uma mensagem para o usuário
+        this.snackBar.open('Erro ao adicionar usuario', 'Fechar', {
+          duration: 2000,
+        });
       }
     });
   }
+
 }
