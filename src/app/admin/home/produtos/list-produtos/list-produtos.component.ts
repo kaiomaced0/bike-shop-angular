17091184698d8aa6f5a8f9c726f846044f7324f9
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-10 06:36:45 -0300

    update c list-produtos

--- src/app/admin/home/produtos/list-produtos/list-produtos.component.ts
+++ src/app/admin/home/produtos/list-produtos/list-produtos.component.ts
@@ -5,6 +5,9 @@ import { Router } from '@angular/router';
 import { Produto } from '../../../../models/produto.model';
 import { HttpClientModule } from '@angular/common/http';
 import { ProdutoService } from '../../../../services/produto/produto.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfiermDialogResetarsenhaComponent } from '../../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
 
 @Component({
   selector: 'app-list-produtos',
@@ -15,7 +18,7 @@ import { ProdutoService } from '../../../../services/produto/produto.service';
 })
 export class ListProdutosComponent {
 
-  constructor(private router: Router, private produtoService: ProdutoService) {
+  constructor(private router: Router, private produtoService: ProdutoService, private dialog: MatDialog, private snackBar: MatSnackBar) {
   }
 
   produtos: Produto[] = [];
@@ -25,16 +28,40 @@ export class ListProdutosComponent {
       this.produtos = data;
     });
   }
+  editar(id:number) {
+    this.router.navigate([`/admin/produtos/edit/${id}`]);
+  }
 
   irParaNewProduto() {
     this.router.navigate(['/admin/produtos/new']);
   }
-  editarProduto(produtoId: number) {
-    this.router.navigate(['/admin/produtos/edit']);
-  }
 
-  excluirProduto(produtoId: number) {
-    this.produtoService.delete(produtoId);
-  }
+
+  deletar(id:number, nome:string){
+    const dialogRef = this.dialog.open(ConfiermDialogResetarsenhaComponent, {
+      width: '250px',
+      data: {
+        message: `Tem certeza que deseja Deletar o Produto de nome: ${nome}?`,
+        n: nome
+      }});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.produtoService.delete(id).subscribe({
+          next: () => {
+            this.snackBar.open('Produto deletado', 'Fechar', {
+              duration: 2000,
+            });
+            this.ngOnInit();
+          },
+          error: (error) => {
+            this.snackBar.open('Erro ao deletar Produto', 'Fechar', {
+              duration: 1000,
+            });
+          }
+        });
+      }
+    });
+    }
 
 }
