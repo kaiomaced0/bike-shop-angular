f2657d1eb79bdca27d6c5d6f9ebf7a2188d8e58f
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-10 06:36:20 -0300

    add c edit-produto

--- src/app/admin/home/produtos/edit-produto/edit-produto.component.ts
+++ src/app/admin/home/produtos/edit-produto/edit-produto.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProdutoService } from '../../../../services/produto/produto.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { Produto } from '../../../../models/produto.model';
 
 @Component({
   selector: 'app-edit-produto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatButton],
   templateUrl: './edit-produto.component.html',
   styleUrl: './edit-produto.component.css'
 })
 export class EditProdutoComponent {
 
+
+  produto: Produto = new Produto();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private service: ProdutoService, private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.service.getById(+id).subscribe({
+        next: (data) => {
+          this.produto = data;
+        },
+        error: (error) => {
+          console.error('Erro ao buscar produto', error);
+        }
+      });
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/admin/produtos']);
+  }
+
+  onSubmit(form: any) {
+    console.log('Dados do Formulário:', form.value);
+  }
+
+  atualizarProduto() {
+    this.service.update(this.produto.id!, this.produto).subscribe({
+      next: () => {
+        console.log('Produto atualizado com sucesso');
+        this.router.navigate(['/admin/produtos']);
+        this.snackBar.open('Produto atualizado', 'Fechar', {
+          duration: 2000,
+        });
+      },
+      error: (erro) => {
+        console.error('Erro ao atualizar Produto:', erro);
+      }
+    });
+  }
 }
