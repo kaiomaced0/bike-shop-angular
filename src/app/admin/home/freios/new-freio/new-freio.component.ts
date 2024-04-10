abddbb490910b1418aba27fad4a3100731344dc9
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-10 06:35:11 -0300

    add new freio

--- src/app/admin/home/freios/new-freio/new-freio.component.ts
+++ src/app/admin/home/freios/new-freio/new-freio.component.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { FreioService } from '../../../../services/freio/freio.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-new-freio',
   standalone: true,
-  imports: [MatFormField, FormsModule],
+  imports: [MatFormField, FormsModule, MatButton],
   templateUrl: './new-freio.component.html',
   styleUrl: './new-freio.component.css'
 })
@@ -15,6 +16,9 @@ export class NewFreioComponent {
 
   nome: string = '';
 
+  cancelar(){
+    this.router.navigate(['/admin/freios']);
+  }
   constructor(private router: Router, private service: FreioService) {}
   adicionarFreio() {
       this.service.insert(this.nome).subscribe({
