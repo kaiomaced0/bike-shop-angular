6623eb9c72033374bc7749874bf14e64adee66c0
05562849259 <kaiomm2000@gmail.com>
2024-04-11 18:12:08 -0300

    ajuste rotas ferramenta

--- src/app/admin/home/ferramentas/ferramentas-routing.module.ts
+++ src/app/admin/home/ferramentas/ferramentas-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListFerramentasComponent } from './list-ferramentas/list-ferramentas.component';
 import { FerramentaService } from '../../../services/ferramenta/ferramenta.service';
 import { ProdutoService } from '../../../services/produto/produto.service';
+import { NewFerramentaComponent } from './new-ferramenta/new-ferramenta.component';
+import { EditFerramentaComponent } from './edit-ferramenta/edit-ferramenta.component';
 
 const routes: Routes = [
-  {path: '', component: ListFerramentasComponent}
+  {path: '', component: ListFerramentasComponent},
+  {path: 'new', component: NewFerramentaComponent},
+  {path: 'edit/:id', component: EditFerramentaComponent}
 ];
 
 @NgModule({
