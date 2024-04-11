d7a6b67365de78190d115ac4fb35a72f6b8ca692
05562849259 <kaiomm2000@gmail.com>
2024-04-11 18:13:29 -0300

    update rota de pneus

--- src/app/admin/home/pneus/pneus-routing.module.ts
+++ src/app/admin/home/pneus/pneus-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListPneusComponent } from './list-pneus/list-pneus.component';
 import { PneuService } from '../../../services/pneu/pneu.service';
 import { NewPneuComponent } from './new-pneu/new-pneu.component';
+import { EditPneuComponent } from './edit-pneu/edit-pneu.component';
 
 const routes: Routes = [
   {path: '', component: ListPneusComponent},
-  {path: 'new', component: NewPneuComponent}
+  {path: 'new', component: NewPneuComponent},
+  {path: 'edit/:id', component: EditPneuComponent}
 ];
 
 @NgModule({
