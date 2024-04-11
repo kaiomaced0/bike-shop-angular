ca4b6294b4be35cca4f5ec9ee258c2b0249e8b65
05562849259 <kaiomm2000@gmail.com>
2024-04-11 18:10:25 -0300

    alteracao list bike

--- src/app/admin/home/bikes/list-bikes/list-bikes.component.ts
+++ src/app/admin/home/bikes/list-bikes/list-bikes.component.ts
@@ -5,6 +5,9 @@ import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { BikeService } from '../../../../services/bike/bike.service';
 import { Bike } from '../../../../models/bike.models';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfiermDialogResetarsenhaComponent } from '../../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
 
 @Component({
   selector: 'app-list-bikes',
@@ -15,7 +18,7 @@ import { Bike } from '../../../../models/bike.models';
 })
 export class ListBikesComponent {
 
-  constructor(private router: Router, private bikeService: BikeService) {
+  constructor(private router: Router, private bikeService: BikeService, private dialog: MatDialog, private snackBar: MatSnackBar) {
   }
 
   bikes: Bike[] = [];
@@ -23,20 +26,43 @@ export class ListBikesComponent {
   ngOnInit() {
     this.bikeService.getAll().subscribe((data: Bike[]) => {
       this.bikes = data;
-
     });
+  }
+  editar(id:number) {
+    this.router.navigate([`/admin/bikes/edit/${id}`]);
   }
 
   irParaNewBike() {
     this.router.navigate(['/admin/bikes/new']);
   }
-  editarBike(produtoId: number) {
-    this.router.navigate([`/admin/bikes/edit/${produtoId}`]);
-  }
 
-  excluirBike(produtoId: number) {
-    // Lógica para excluir o produto
-  }
+
+  deletar(id:number, nome:string){
+    const dialogRef = this.dialog.open(ConfiermDialogResetarsenhaComponent, {
+      width: '250px',
+      data: {
+        message: `Tem certeza que deseja Deletar a Bike de nome: ${nome}?`,
+        n: nome
+      }});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.bikeService.delete(id).subscribe({
+          next: () => {
+            this.snackBar.open('Bike deletada', 'Fechar', {
+              duration: 2000,
+            });
+            this.ngOnInit();
+          },
+          error: (error) => {
+            this.snackBar.open('Erro ao deletar Bike', 'Fechar', {
+              duration: 1000,
+            });
+          }
+        });
+      }
+    });
+    }
 
 
 }
