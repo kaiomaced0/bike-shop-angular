39a136aeed973ac6990fe605747340162c76077d
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-12 04:07:12 -0300

    ajustes lista de marcas

--- src/app/admin/home/marca/list-marcas/list-marcas.component.ts
+++ src/app/admin/home/marca/list-marcas/list-marcas.component.ts
@@ -5,6 +5,9 @@ import { MatIcon } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { MarcaService } from '../../../../services/marca/marca.service';
 import { Marca } from '../../../../models/marca.model';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfiermDialogResetarsenhaComponent } from '../../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
 
 @Component({
   selector: 'app-list-marcas',
@@ -16,25 +19,51 @@ import { Marca } from '../../../../models/marca.model';
 export class ListMarcasComponent {
 
 
-  constructor(private router: Router, private service: MarcaService) {
+  constructor(private router: Router, private MarcaService: MarcaService, private dialog: MatDialog, private snackBar: MatSnackBar) {
+    this.ngOnInit();
   }
 
   marcas: Marca[] = [];
 
   ngOnInit() {
-    this.service.getAll().subscribe((data: Marca[]) => {
+    this.MarcaService.getAll().subscribe((data: Marca[]) => {
       this.marcas = data;
     });
   }
+  editar(id:number) {
+    this.router.navigate([`/admin/marcas/edit/${id}`]);
+  }
 
-  irParaNewmarca() {
+  irParaNewMarca() {
     this.router.navigate(['/admin/marcas/new']);
   }
-  editarmarca(marcaId: number) {
-    this.router.navigate(['/admin/marcas/edit']);
-  }
 
-  excluirmarca(marcaId: number) {
-  }
+
+  deletar(id:number, nome:string){
+    const dialogRef = this.dialog.open(ConfiermDialogResetarsenhaComponent, {
+      width: '250px',
+      data: {
+        message: `Tem certeza que deseja Deletar a Marca de nome: ${nome}?`,
+        n: nome
+      }});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.MarcaService.delete(id).subscribe({
+          next: () => {
+            this.snackBar.open('Marca deletada', 'Fechar', {
+              duration: 2000,
+            });
+            this.ngOnInit();
+          },
+          error: (error) => {
+            this.snackBar.open('Erro ao deletar Marca', 'Fechar', {
+              duration: 1000,
+            });
+          }
+        });
+      }
+    });
+    }
 
 }
