c3e898710117c5b7d96ae407bf029eee67ad17e1
05562849259 <kaiomm2000@gmail.com>
2024-04-17 16:54:18 -0300

    .

--- src/app/admin/home/home/home.component.ts
+++ src/app/admin/home/home/home.component.ts
@@ -14,7 +14,6 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   constructor(private router: Router) {
-    auth.login('kaio', '123')
   }
 
   irParaProdutosAdmin() {
