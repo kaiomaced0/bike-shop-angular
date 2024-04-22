8c7de6fa251c3436b1879f47c90714bd32b78d4b
05562849259 <kaiomm2000@gmail.com>
2024-04-22 16:57:14 -0300

    add auth service para loar e alter icons css d fav

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = '/auth';
+
+  constructor(private http: HttpClient) { }
+
+  private token  = localStorage.getItem('token');
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + this.token})
+  };
+  httpOptions2 = {
+    headers: new HttpHeaders({'Authorization': 'Bearer ' + this.token})
+  };
+
+
+  login(login: string, senha: string): Observable<any> {
+    const body = { login, senha };
+    return this.http.post<any>(this.baseUrl, body, { observe: 'response' });
+}
+
+}
