e863d6802b916c17afe1dba623ce8f4e6c1d8347
05562849259 <kaiomm2000@gmail.com>
2024-05-15 17:22:05 -0300

    mudando ordem para alfabetica

--- proxy.conf.js
+++ proxy.conf.js
@@ -6,6 +6,66 @@ const PROXY_CONF = [
     logLevel: 'debug'
   },
   {
+    context: ['/bike'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/categoria'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/cidade'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/compra'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/cupom'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/ferramenta'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/freio'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/marca'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/pessoafisica'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
+    context: ['/pneu'],
+    target: 'http://localhost:8080/',
+    secure: false,
+    logLevel: 'debug'
+  },
+  {
     context: ['/produto'],
     target: 'http://localhost:8080/',
     secure: false,
@@ -18,67 +78,7 @@ const PROXY_CONF = [
     logLevel: 'debug'
   },
   {
-    context: ['/bike'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
     context: ['/usuario'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/pessoafisica'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/ferramenta'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/marca'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/cidade'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/pneu'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/freio'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/cupom'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/categoria'],
-    target: 'http://localhost:8080/',
-    secure: false,
-    logLevel: 'debug'
-  },
-  {
-    context: ['/compra'],
     target: 'http://localhost:8080/',
     secure: false,
     logLevel: 'debug'
