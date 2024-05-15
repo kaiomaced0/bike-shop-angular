4d43578a50690606bdce178c32312773cf5cf23c
05562849259 <kaiomm2000@gmail.com>
2024-05-15 19:08:51 -0300

    adicionando marca e categoria a edit, n finalizado

--- src/app/admin/home/produtos/edit-produto/edit-produto.component.ts
+++ src/app/admin/home/produtos/edit-produto/edit-produto.component.ts
@@ -1,34 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProdutoService } from '../../../../services/produto/produto.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { Produto } from '../../../../models/produto.model';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfiermDialogResetarsenhaComponent } from '../../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
+import { CategoriaService } from '../../../../services/categoria/categoria.service';
+import { MarcaService } from '../../../../services/marca/marca.service';
+import { MatSelectModule } from '@angular/material/select';
+import { Categoria } from '../../../../models/categoria.model';
+import { Marca } from '../../../../models/marca.model';
 
 @Component({
   selector: 'app-edit-produto',
   standalone: true,
-  imports: [FormsModule, MatButton],
+  imports: [FormsModule, MatButton, MatSelectModule, ReactiveFormsModule],
   templateUrl: './edit-produto.component.html',
   styleUrl: './edit-produto.component.css'
 })
-export class EditProdutoComponent {
+export class EditProdutoComponent implements OnInit {
 
 
   produto: Produto = new Produto();
+  categorias: Categoria[] = [];
+  marcas: Marca[] = [];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private service: ProdutoService,
     private snackBar: MatSnackBar,
-    private dialog: MatDialog
+    private dialog: MatDialog, private categoriaService: CategoriaService, private marcaService: MarcaService
   ) {}
 
   ngOnInit(): void {
+    this.produto.categoriasId = [];
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
       this.service.getById(+id).subscribe({
@@ -40,6 +48,26 @@ export class EditProdutoComponent {
         }
       });
     }
+    this.categoriaService.getAll().subscribe({
+      next: (categorias) => {
+        this.categorias = categorias;
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar categorias', erro);
+      }
+    });
+    this.marcaService.getAll().subscribe({
+      next: (marcas) => {
+        this.marcas = marcas;
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar marcas', erro);
+      }
+    });
+    this.produto.categorias?.forEach(element => {
+      this.produto.categoriasId?.push(element.id!);
+    });
+    this.produto.idMarca = this.produto.marca?.id;
   }
 
   cancelar(){
