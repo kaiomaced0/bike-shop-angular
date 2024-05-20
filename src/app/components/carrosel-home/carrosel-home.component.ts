6731da3b102bb335b73784be6f39e2f0c0df45e0
kaiomaced0 <kaiomm2000@gmail.com>
2024-05-20 00:23:15 -0300

    ajustes do carrossel

--- src/app/components/carrosel-home/carrosel-home.component.ts
+++ src/app/components/carrosel-home/carrosel-home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeService } from '../../services/home/home.service';
+import { Carrossel } from '../../models/carrossel.model';
 
 @Component({
   selector: 'app-carrosel-home',
@@ -7,14 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './carrosel-home.component.html',
   styleUrl: './carrosel-home.component.css'
 })
-export class CarroselHomeComponent {
+export class CarroselHomeComponent implements OnInit {
   currentSlide: number = 0;
   slideInterval: any;
 
-  constructor() {
+  constructor(private router: Router, private service: HomeService) {
     this.startSlideShow();
   }
-
+  carrossels: Carrossel[] = [];
+  ngOnInit() {
+    this.service.carrossel().subscribe((data: Carrossel[]) => {
+      this.carrossels = data;
+    });
+  }
   moveSlide(direction: number): void {
     this.currentSlide += direction;
     this.currentSlide = (this.currentSlide + 3) % 3; // Assumindo 3 slides
