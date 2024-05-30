e4dd7915f7c996fc9545a0eef7e542d7f6c58b09
kaiomaced0 <kaiomm2000@gmail.com>
2024-05-30 20:26:24 -0300

    c dados update

--- src/app/home/conta/dados/dados.component.ts
+++ src/app/home/conta/dados/dados.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariologadoService } from '../../../usuariologado/usuariologado.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+import { Usuario } from '../../../models/usuario.model';
 
 @Component({
   selector: 'app-dados',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './dados.component.html',
   styleUrl: './dados.component.css'
 })
-export class DadosComponent {
+export class DadosComponent implements OnInit {
+  usuario?: Usuario;
+
+  constructor(private usuarioLogadoService: UsuariologadoService, private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.usuarioLogadoService.getUsuarioLogado().subscribe(
+      data => {
+        this.usuario = data;
+      },
+      error => {
+        console.error('Erro ao carregar dados do usuário', error);
+      }
+    );
+  }
+
 
 }
