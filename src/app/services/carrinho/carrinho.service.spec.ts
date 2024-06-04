b2b03e1c1260bca94b0ccc0396fd7034d7f546cd
05562849259 <kaiomm2000@gmail.com>
2024-06-04 20:05:13 -0300

    substituido o service de carrinho e add func reais

--- src/app/services/carrinho/carrinho.service.spec.ts
+++ src/app/services/carrinho/carrinho.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CartService } from './cart.service';
+import { CarrinhoService } from './carrinho.service';
 
-describe('CartService', () => {
-  let service: CartService;
+describe('CarrinhoService', () => {
+  let service: CarrinhoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CartService);
+    service = TestBed.inject(CarrinhoService);
   });
 
   it('should be created', () => {
---
+++ src/app/services/carrinho/carrinho.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { ItemCompra } from '../../models/itemcompra.models';
+import { CompraItemCompra } from '../../models/compraitemcompra.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarrinhoService {
+
+  private cartKey = 'cart';
+  private carrinho: ItemCompra[] = [];
+
+  constructor() {
+    this.loadCart();
+  }
+
+  private saveCart(): void {
+    localStorage.setItem(this.cartKey, JSON.stringify(this.carrinho));
+  }
+
+  private loadCart(): void {
+    const cart = localStorage.getItem(this.cartKey);
+    if (cart) {
+      this.carrinho = JSON.parse(cart);
+    }
+  }
+
+  adicionarProduto(produtoId: number): void {
+    this.loadCart();
+    const item = this.carrinho.find(p => p.produtoId === produtoId);
+    if (item) {
+      item.quantidade!++;
+    } else {
+      this.carrinho.push({ produtoId, quantidade: 1 });
+    }
+    this.saveCart();
+  }
+
+  aumentarQuantidade(produtoId: number): void {
+    this.loadCart();
+    const item = this.carrinho.find(p => p.produtoId === produtoId);
+    if (item) {
+      item.quantidade!++;
+    }
+    this.saveCart();
+  }
+
+  diminuirQuantidade(produtoId: number): void {
+    this.loadCart();
+    const item = this.carrinho.find(p => p.produtoId === produtoId);
+    if (item && item.quantidade! > 0) {
+      item.quantidade!--;
+      if (item.quantidade === 0) {
+        this.removerProduto(produtoId);
+      } else {
+        this.saveCart();
+      }
+    }
+  }
+
+  removerProduto(produtoId: number): void {
+    this.loadCart();
+    this.carrinho = this.carrinho.filter(p => p.produtoId !== produtoId);
+    this.saveCart();
+    window.location.reload();
+  }
+
+  getQuantidade(produtoId: number): number {
+    this.loadCart();
+    const item = this.carrinho.find(p => p.produtoId === produtoId);
+    if (item) {
+      return item.quantidade!;
+    }
+    else{
+      return 0;
+    }
+  }
+
+  getCarrinho(): ItemCompra[] {
+    this.loadCart();
+    return this.carrinho;
+  }
+
+  limparCarrinho(): void {
+    this.carrinho = [];
+    this.saveCart();
+  }
+
+  enviarCompra(idCupom?: number, idEndereco?: number): CompraItemCompra {
+    const compra: CompraItemCompra = {
+      listaIemCompra: this.carrinho,
+      idCupom: idCupom,
+      idEndereco: idEndereco,
+      formaPagamento: 1
+    };
+    this.limparCarrinho();
+    return compra;
+  }
+}
