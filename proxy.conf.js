9f28ac9d5fd378cc58d16be00daeb972efe5e0f3
05562849259 <kaiomm2000@gmail.com>
2024-06-05 16:38:12 -0300

    add proxy da vps

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,100 +1,201 @@
 const PROXY_CONF = [
   {
     context: ['/auth'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/bike'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/categoria'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/cidade'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/compra'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/cupom'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/ferramenta'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/freio'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/marca'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/pessoafisica'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/pneu'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/produto'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/produto/admin'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/usuario'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/usuariologado'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   },
   {
     context: ['/home'],
-    target: 'http://localhost:8080/',
+    target: 'http://34.151.200.157:8080/',
     secure: false,
     logLevel: 'debug'
   }
 ];
 
 module.exports = PROXY_CONF;
+
+// const PROXY_CONF = [
+//   {
+//     context: ['/auth'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/bike'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/categoria'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/cidade'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/compra'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/cupom'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/ferramenta'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/freio'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/marca'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/pessoafisica'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/pneu'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/produto'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/produto/admin'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/usuario'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/usuariologado'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   },
+//   {
+//     context: ['/home'],
+//     target: 'http://localhost:8080/',
+//     secure: false,
+//     logLevel: 'debug'
+//   }
+// ];
+
+// module.exports = PROXY_CONF;
