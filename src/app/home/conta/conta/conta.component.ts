ea07221eb05df3049df376ede65fc6a0d0305ad5
kaiomaced0 <kaiomm2000@gmail.com>
2024-06-07 06:42:48 -0300

    dados e conta atualizados

--- src/app/home/conta/conta/conta.component.ts
+++ src/app/home/conta/conta/conta.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { UsuariologadoService } from '../../../services/usuariologado/usuariologado.service';
 import { Usuario } from '../../../models/usuario.model';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-conta',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormField, MatButtonModule, MatInputModule, MatLabel],
   templateUrl: './conta.component.html',
   styleUrl: './conta.component.css'
 })
 export class ContaComponent implements OnInit {
   usuario?: Usuario;
+  tell?: string[] = [];
 
   constructor(private router: Router, private usuarioLogadoService: UsuariologadoService) { }
 
@@ -19,6 +23,8 @@ export class ContaComponent implements OnInit {
     this.usuarioLogadoService.getUsuarioLogado().subscribe(
       data => {
         this.usuario = data;
+        this.tell =this.usuario.telefones;
+        console.log(this.tell);
       },
       error => {
         console.error('Erro ao carregar dados do usuário', error);
--- src/app/home/conta/dados/dados.component.ts
+++ src/app/home/conta/dados/dados.component.ts
@@ -3,18 +3,28 @@ import { UsuariologadoService } from '../../../services/usuariologado/usuariolog
 import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { Usuario } from '../../../models/usuario.model';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButton } from '@angular/material/button';
+import { ConfiermDialogResetarsenhaComponent } from '../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { pessoateste, UpdateDados } from '../../../models/updatedados.model';
 
 @Component({
   selector: 'app-dados',
   standalone: true,
-  imports: [MatIcon],
+  imports: [FormsModule, MatButton, MatSelectModule, ReactiveFormsModule],
   templateUrl: './dados.component.html',
   styleUrl: './dados.component.css'
 })
 export class DadosComponent implements OnInit {
   usuario?: Usuario;
+  udados?: UpdateDados;
+  upessoa?: pessoateste;
 
-  constructor(private usuarioLogadoService: UsuariologadoService, private dialog: MatDialog) { }
+  constructor(private usuarioLogadoService: UsuariologadoService, private dialog: MatDialog, private router: Router,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.usuarioLogadoService.getUsuarioLogado().subscribe(
@@ -25,7 +35,50 @@ export class DadosComponent implements OnInit {
         console.error('Erro ao carregar dados do usuário', error);
       }
     );
+    this.udados!.senhaAtual! = '';
+    this.upessoa!.nome = '';
+    this.upessoa!.login = '';
+    this.upessoa!.email = '';
+    this.upessoa!.cpf = '';
+    this.upessoa!.senha = '';
+    this.udados!.pessoa = this.upessoa!;
   }
 
+  atualizar() {
+    this.udados!.senhaAtual! = this.usuario!.senhaAtual!;
+    this.upessoa!.nome = this.usuario?.nome;
+    this.upessoa!.login = this.usuario?.login;
+    this.upessoa!.email = this.usuario?.email;
+    this.upessoa!.cpf = this.usuario?.cpf;
+    this.upessoa!.dataNascimento = this.usuario?.dataNascimento;
+    this.upessoa!.senha = this.usuario?.senha;
+    this.udados!.pessoa = this.upessoa!;
+    const dialogRef = this.dialog.open(ConfiermDialogResetarsenhaComponent, {
+      width: '250px',
+      data: {
+        message: `Tem certeza que deseja atualizar seus dados?`,
+      }});
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.usuarioLogadoService.dadosupdate(this.udados!).subscribe({
+            next: () => {
+              console.log('Usuario atualizado com sucesso');
+              this.router.navigate(['/dados']);
+              this.snackBar.open('Dados atualizados', 'Fechar', {
+                duration: 3000,
+              });
+            },
+            error: (erro) => {
+              console.error('Erro ao atualizar Dados:', erro);
+            }
+          });
+        }
+
+
+  });}
+
+  cancelar(){
+    this.router.navigate(['/conta']);
+  }
 
 }
