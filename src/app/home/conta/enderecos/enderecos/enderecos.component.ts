ff74c35452910f509822af1f5e7e196d607f07ae
kaiomaced0 <kaiomm2000@gmail.com>
2024-06-07 06:41:44 -0300

    add lista de enderecos

--- src/app/home/conta/enderecos/enderecos/enderecos.component.ts
+++ src/app/home/conta/enderecos/enderecos/enderecos.component.ts
@@ -1,45 +1,46 @@
-import { Component } from '@angular/core';
-import { EnderecoItemComponent } from '../../../components/endereco-item/endereco-item.component';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { Produto } from '../../../../models/produto.model';
+import { HttpClientModule } from '@angular/common/http';
+import { ProdutoService } from '../../../../services/produto/produto.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfiermDialogResetarsenhaComponent } from '../../../../components/dialog/confierm-dialog-resetarsenha/confierm-dialog-resetarsenha.component';
+import { EnderecoService } from '../../../../services/endereco/endereco.service';
+import { UsuariologadoService } from '../../../../services/usuariologado/usuariologado.service';
+import { Endereco } from '../../../../models/endereco.models';
 
 @Component({
   selector: 'app-enderecos',
   standalone: true,
-  imports: [EnderecoItemComponent],
+  imports: [MatIcon, MatButton, HttpClientModule],
   templateUrl: './enderecos.component.html',
   styleUrl: './enderecos.component.css'
 })
 export class EnderecosComponent {
 
-  enderecos = [
-    {
-      id: '1',
-      nome: 'Casa',
-      cidade: 'São Paulo',
-      cep: '01000-000',
-      complemento: 'Apartamento 101',
-      numero: '123'
-    },
-    {
-      id: '2',
-      nome: 'Trabalho',
-      cidade: 'Rio de Janeiro',
-      cep: '20000-000',
-      complemento: 'Sala 201',
-      numero: '456'
-    },
-    {
-      id: '3',
-      nome: 'Casa de Praia',
-      cidade: 'Salvador',
-      cep: '40000-000',
-      complemento: 'Casa',
-      numero: '789'
-    }
-  ];
 
-  definirComoPadrao(id: string) {
-    console.log("Definindo como endereço padrão:", id);
-    // Implemente a lógica para definir o endereço como padrão aqui
+  constructor(private router: Router, private usuarioLogadoService: UsuariologadoService, private dialog: MatDialog, private snackBar: MatSnackBar) {
   }
+
+  enderecos: Endereco[] = [];
+
+  ngOnInit() {
+    this.usuarioLogadoService.getEnderecos().subscribe((data: Produto[]) => {
+      this.enderecos = data;
+    });
+  }
+  editar(id:number) {
+    this.router.navigate([`/conta/enderecos/edit/${id}`]);
+  }
+
+  new() {
+    this.router.navigate(['/conta/enderecos/add']);
+  }
+  principal(id:number){
+
+  }
+
 }
