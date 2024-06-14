5f745e04ed91116032fe7834e0e291a7bc6039d3
kaiomaced0 <kaiomm2000@gmail.com>
2024-06-13 22:43:34 -0300

    .

--- src/app/services/produto/produto.service.ts
+++ src/app/services/produto/produto.service.ts
@@ -8,8 +8,8 @@ import { Produto } from '../../models/produto.model';
   providedIn: 'root'
 })
 export class ProdutoService {
-  // private apiUrl = 'http://localhost:8080/produto';
-  private apiUrl = 'http://172.19.0.4:8080/produto';
+  private apiUrl = 'http://localhost:8080/produto';
+  // private apiUrl = 'http://172.19.0.4:8080/produto';
 
   private token  = localStorage.getItem('token');
 
